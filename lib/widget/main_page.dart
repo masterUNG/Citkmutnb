d1d737ecfa95899fb3087d4108a1d3881572e867
nokweed <nokweed@gmail.com>
2020-06-06 15:58:11 +0700

    update 6june

--- lib/widget/main_page.dart
+++ lib/widget/main_page.dart
@@ -1,5 +1,6 @@
+import 'package:citkmutnb/page/history.dart';
+import 'package:citkmutnb/utility/my_style.dart';
 import 'package:flutter/material.dart';
-
 
 class MainPage extends StatefulWidget {
   @override
@@ -9,8 +10,53 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'Main Page'
+    return Stack(
+      children: <Widget>[
+        Mystyle().myBG(),
+        Column(
+          children: <Widget>[
+            mySizebox(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                createItem('item1', History()),
+                createItem('item2', History()),
+                createItem('item3', History())
+              ],
+            ),
+            mySizebox(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                createItem('item4', History()),
+                createItem('item5', History()),
+                createItem('item6', History())
+              ],
+            ),
+            mySizebox(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                createItem('item7', History()),
+              ],
+            )
+          ],
+        ),
+      ],
     );
   }
+
+  SizedBox mySizebox() {
+    return SizedBox(
+      height: 24.0,
+    );
+  }
+
+  Widget createItem(String nameImage, Widget widget) => GestureDetector(
+        onTap: () {
+          MaterialPageRoute route = MaterialPageRoute(builder: (context) => widget,);
+          Navigator.push(context, route);
+        },
+        child: Image.asset('images/$nameImage.png'),
+      );
 }
