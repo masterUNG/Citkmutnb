c26c97659864cf71b17817eefa430a9f118bdb3b
nokweed <nokweed@gmail.com>
2020-06-20 23:26:20 +0700

    update20-6-63 23.26

--- lib/widget/department_page.dart
+++ lib/widget/department_page.dart
@@ -13,43 +13,50 @@ class _DepartmentpageState extends State<Departmentpage> {
       children: <Widget>[
         Column(mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            createimg(('Pak1')),
-            createimg(('Pak2')),   
+            createimg(('Pak1'),Pak1()),
+            createimg(('Pak2'),Pak1()),   
           ],
         ),
         Column(mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            createimg(('Pak3')),
-            createimg(('Pak4')),
+            createimg(('Pak3'),Pak1()),
+            createimg(('Pak4'),Pak1()),
           ],
         ),
         Column(mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            createimg(('Pak5')),
-            createimg(('Pak6')),
+            createimg(('Pak5'),Pak1()),
+            createimg(('Pak6'),Pak1()),
           ],
         ),
         Column(mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            createimg(('Pak7')),
-            createimg(('Pak8')),
+            createimg(('Pak7'),Pak1()),
+            createimg(('Pak8'),Pak1()),
           ],
         ),
         Column(mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            createimg(('Pak9')),
-            createimg(('Pak10')),
+            createimg(('Pak9'),Pak1()),
+            createimg(('Pak10'),Pak1()),
           ],
         ),
       ],
     );
   }
 
-  Widget createimg(String nameimg) {
-    return Container(
-      width: 300.0,
-      height: 200.0,
-      child: Image.asset('images/$nameimg.png'),
-    );
-  }
+  Widget createimg(String nameimg, Widget widget) => GestureDetector(
+        onTap: () {
+          MaterialPageRoute route = MaterialPageRoute(builder: (context) => widget,);
+          Navigator.push(context, route);
+        },
+        child: Container(child: Image.asset('images/$nameimg.png'),width: 300, height: 200,),
+      );
+  // {FlatButton(onPressed: null, child: null)
+  //   return Container(
+  //     width: 300.0,
+  //     height: 200.0,
+  //     child: Image.asset('images/$nameimg.png'),
+  //   );
+  // }
 }
